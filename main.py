5a775b022658d7631ba1db0cbc6beea9f2910944
Tim-U-12 <tim.a.ung.12@gmail.com>
2024-04-15 16:36:22 +1000

    demonstrate several ways to select columns of interest

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ import pandas as pd
 if __name__ == "__main__":
     melb_file_path = './datasets/melb_data.csv'
     melb_data = pd.read_csv(melb_file_path)
-    print(melb_data.describe())
+    
+    # Selecting all of the column names
+    melb_data_cols = melb_data.columns
+    
+    # Selecting the specific values in a column
+    melb_data_prices = melb_data.Price
+
+    # Choosing features
+    melb_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+    melb_features_data = melb_data[melb_features]
